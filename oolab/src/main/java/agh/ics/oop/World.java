53e40f26004b2b1fc1ec8eba608c91be84531251
Piotr Kacprzak <piotrkac4@gmail.com>
2024-10-14 16:14:32 +0200

    Task 15 done

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -7,10 +7,24 @@ public class World {
         System.out.println("system zakończył działanie");
     }
     public static void run(String[] args){
-        System.out.println("zwierzak wystartował");
+        System.out.println("START");
         for(int i = 0; i < args.length-1; i++) {
             System.out.print(args[i] + ", ");
         }
         System.out.println(args[args.length-1]);
+
+        for(String arg : args) {
+            String message = switch (arg){
+                case "l" -> "skręca w lewo";
+                case "p" -> "skręca w prawo";
+                case "f" -> "idzie do przodu";
+                case "b" -> "idzie do tyłu";
+                default -> null;
+            };
+            if(message != null) {
+                System.out.println("Zwierzak " + message);
+            }
+        }
+        System.out.println("STOP");
     }
 }
