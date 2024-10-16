4795952d9d156d81e839e9cc584f239feba637f1
Piotr Kacprzak <piotrkac4@gmail.com>
2024-10-16 14:18:47 +0200

    Tasks to 17 done

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,30 +1,46 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MoveDirection;
+
 public class World {
     public static void main(String[] args) {
         System.out.println("system wystartował");
-        run(args);
+//        run(args);
+        run(OptionsParser.DirectionParser(args));
         System.out.println("system zakończył działanie");
     }
-    public static void run(String[] args){
-        System.out.println("START");
-        for(int i = 0; i < args.length-1; i++) {
-            System.out.print(args[i] + ", ");
-        }
-        System.out.println(args[args.length-1]);
+    public static void run(/*String[] args*/MoveDirection[] directions) {
+        System.out.println("Start");
+//        for(int i = 0; i < args.length-1; i++) {
+//            System.out.print(args[i] + ", ");
+//        }
+//        System.out.println(args[args.length-1]);
 
-        for(String arg : args) {
-            String message = switch (arg){
-                case "l" -> "skręca w lewo";
-                case "p" -> "skręca w prawo";
-                case "f" -> "idzie do przodu";
-                case "b" -> "idzie do tyłu";
+//        for(String arg : args) {
+//            String message = switch (arg){
+//                case "l" -> "skręca w lewo";
+//                case "p" -> "skręca w prawo";
+//                case "f" -> "idzie do przodu";
+//                case "b" -> "idzie do tyłu";
+//                default -> null;
+//            };
+//            if(message != null) {
+//                System.out.println("Zwierzak " + message);
+//            }
+//        }
+
+        for (MoveDirection direction : directions) {
+            String message = switch (direction){
+                case LEFT -> "skręca w lewo";
+                case RIGHT -> "skręca w prawo";
+                case FORWARD -> "idzie do przodu";
+                case BACKWARD -> "idzie do tyłu";
                 default -> null;
             };
-            if(message != null) {
-                System.out.println("Zwierzak " + message);
-            }
+            System.out.println("Zwierzak" + message);
         }
-        System.out.println("STOP");
+
+
+        System.out.println("Stop");
     }
 }
