9270d20525c611e4abdd2f0f57fb70221a9307e9
Piotr Kacprzak <piotrkac4@gmail.com>
2024-10-21 15:23:14 +0200

    Task 4 done

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,13 +1,20 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
 
 public class World {
     public static void main(String[] args) {
-        System.out.println("system wystartował");
-//        run(args);
-        run(OptionsParser.DirectionParser(args));
-        System.out.println("system zakończył działanie");
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+
+//        System.out.println("system wystartował");
+////        run(args);
+//        run(OptionsParser.DirectionParser(args));
+//        System.out.println("system zakończył działanie");
     }
     public static void run(/*String[] args*/MoveDirection[] directions) {
         System.out.println("Start");
